eb1c65bb00253abd65c5de7cb34d3155c24663d6
vnz29 <vince.great29@gmail.com>
2025-05-11 16:38:50 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
